7ef1cf14a8aa401cac95fc362c605569b5dcf69b
Agustín Escobar <83258872+Nitsugae@users.noreply.github.com>
2021-07-06 23:46:47 -0300

    Add files via upload

---
+++ assets/js/mainAportes.js
@@ -0,0 +1,36 @@
+traerAportes(mostrarAportes);
+
+function traerAportes(callback) {
+    let url = "http://localhost:5000";
+    let xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let aportes = JSON.parse(this.responseText);
+            callback(aportes);
+        }
+
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+
+}
+
+function mostrarAportes(aportes) {
+
+    for (let i = 0; i < 12; i++) {
+        document.getElementById("cards-aportes").innerHTML += `<div class="col-lg-3 col-md-6 d-flex separar mt-3">
+            <div class="mx-auto d-flex id="single-card">
+                <div class="card"  style="width: 18rem;">
+                    <img src="${aportes.apuntes[i].imagenApunte}" class="card-img-top" alt="..." style="object-fit:cover !important;" >
+                    <div class="card-body" >
+                        <h5 class="card-title">${aportes.apuntes[i].nombre}</h5>
+                        <p class="card-text">Materia:${aportes.apuntes[i].apunteMateria}</p>
+                        <a href="#" class="btn btn-primary">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+
+}
